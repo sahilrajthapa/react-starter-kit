223ff1b4e373b84906c094261614c1cd89ba00b8
srthapa <srthapa_611@yahoo.com>
2020-05-23 18:59:56 +0545

    Add eslint and prettier

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -25,7 +25,7 @@ module.exports = (env) => {
           {
             test: /\.js$/,
             exclude: /node_modules/,
-            use: ['babel-loader'],
+            use: ['babel-loader', 'eslint-loader'],
           },
           {
             test: /\.css$/,
